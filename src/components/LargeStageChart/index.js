038d044805894c4f3e728f77b6524a19e5db5a61
Libby Duggan <lduggan@ESP.COM>
2019-11-19 11:17:28 -0500

    added ref area labels

--- src/components/LargeStageChart/index.js
+++ src/components/LargeStageChart/index.js
@@ -34,11 +34,11 @@ export default class Example extends PureComponent {
         {/* <Tooltip color="#444444"/> */}
         <Line type="monotone" data={this.props.data} dataKey="value" dot={false} stroke="#111111" name="actuals" key="actuals"  strokeOpacity={1}/>
         <Line type="monotone" data={this.props.forecastData} dataKey="value" dot={false} stroke="#FF0000" name="forecast" key="forecast" strokeOpacity={1}/>
-        <ReferenceArea y1={0} y2={this.props.levels.normal} alwaysShow fill="#28a745" strokeOpacity={0.3} />
-        <ReferenceArea y1={this.props.levels.normal} y2={this.props.levels.monitor} alwaysShow fill="#ffc107" strokeOpacity={0.3} />
-        <ReferenceArea y1={this.props.levels.monitor} y2={this.props.levels.minor} alwaysShow fill="#ff9007" strokeOpacity={0.3} />
-        <ReferenceArea y1={this.props.levels.minor} y2={this.props.levels.moderate} alwaysShow fill="#dc3545" strokeOpacity={0.3} />
-        <ReferenceArea y1={this.props.levels.moderate} y2={this.props.levels.major} alwaysShow fill="#7626bf" strokeOpacity={0.3} />
+        <ReferenceArea y1={0} y2={this.props.levels.normal} alwaysShow fill="#28a745" strokeOpacity={0.3} label={{value: "Normal", fontSize: 9, position: 'insideTopRight'}}/>
+        <ReferenceArea y1={this.props.levels.normal} y2={this.props.levels.monitor} alwaysShow fill="#ffc107" strokeOpacity={0.3} label={{value: "Monitor", fontSize: 9, position: 'insideTopRight'}}/>
+        <ReferenceArea y1={this.props.levels.monitor} y2={this.props.levels.minor} alwaysShow fill="#ff9007" strokeOpacity={0.3} label={{value: "Minor", fontSize: 9, position: 'insideTopRight'}}/>
+        <ReferenceArea y1={this.props.levels.minor} y2={this.props.levels.moderate} alwaysShow fill="#dc3545" strokeOpacity={0.3} label={{value: "Moderate", fontSize: 9, position: 'insideTopRight'}}/>
+        <ReferenceArea y1={this.props.levels.moderate} y2={this.props.levels.major} alwaysShow fill="#7626bf" strokeOpacity={0.3} label={{value: "Major", fontSize: 9, position: 'insideTopRight'}}/>
       </LineChart>
     );
   }
