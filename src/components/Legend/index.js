84c38fefd3ca32001aeeea2817d41cd4e6151048
Teddy Zenebe <tzenebe@ESP.COM>
2019-11-22 15:27:00 -0500

    onclick functionality and css added to the gage level

--- src/components/Legend/index.js
+++ src/components/Legend/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {useState} from 'react'
 import Current from '../../images/allpins/legend/CURRENT.png';
 import Forecast from '../../images/allpins/legend/FORECAST.png';
 import Normal from '../../images/allpins/legend/GXXX.png';
@@ -14,35 +15,44 @@ import Constant from '../../images/allpins/legend/BXCX.png';
 
 const Legend = () => {  
 
+    const [ currentVisiblity, setvisiblity ] = useState({visibility: "visible"});
+
+    function toggleLegend(e){
+        e.preventDefault()
+        setvisiblity({
+            visibility: "hidden"
+          })
+    }
+
     return (
 
-        <div className="Lagend">
-            <h4>Gage Level</h4>
+        <div className="lagend" style={currentVisiblity} >
+            <h5 className="Main-Legend-Titel"><strong>Gage Level</strong> <span className="toggle-Legend" onClick={toggleLegend} >X</span></h5>
             <div>
-                <h5>Gage Symbols</h5>
+                <h6 className="small-Legend-Titel" ><strong>Gage Symbols</strong></h6>
                 <div>
-                    <img src={Current} /><br/>
-                    <img src={Forecast} /><br/>
+                    <img src={Current} alt="Current" /> <small>Current Condition</small><br/>
+                    <img src={Forecast} alt="Forecast" /> <small>Forecast Peak Condition</small><br/>
                 </div>
             </div>
             <div>
-                <h5>Risk Ratings</h5>
+                <h6 className="small-Legend-Titel" ><strong>Risk Ratings</strong></h6>
                 <div>
-                    <img src={Normal} /> <br/>
-                    <img src={Monitor} /><br/>
-                    <img src={Minor} /><br/>
-                    <img src={Moderate} /><br/>
-                    <img src={Major} /><br/>
-                    <img src={NotRisk} /><br/>
-                    <img src={OutofService} /><br/>
+                    <img src={Normal} alt="Normal"/> <small>Normal</small> <br/>
+                    <img src={Monitor} alt="Monitore" /> <small>Monitor</small><br/>
+                    <img src={Minor} alt="Minor" /> <small>Minor Flooding</small><br/>
+                    <img src={Moderate} alt="Moderate" /> <small>Moderate Flooding</small><br/>
+                    <img src={Major} alt="Major" /> <small>Major Flooding</small><br/>
+                    <img src={NotRisk} alt="NotRisk" /> <small>Not Risk Rated</small><br/>
+                    <img src={OutofService} alt="OutofService" /> <small>Out Of Service</small><br/>
                 </div>
             </div>
             <div>
-                <h5>Trend</h5>
+                <h6 className="small-Legend-Titel"><strong>Trend</strong></h6>
                 <div>
-                    <img src={Rising} /><br/>
-                    <img src={Falling} /><br/>
-                    <img src={Constant} />
+                    <img src={Rising} alt="Rising" /> <small>Rising</small><br/>
+                    <img src={Falling} alt="Falling" /> <small>Falling</small><br/>
+                    <img src={Constant} alt="Constant" /> <small>Constant</small>
                 </div>
             </div>
         </div>
