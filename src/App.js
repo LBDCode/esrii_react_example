c2d358e248bd3f15d8a10f9ba172d290d7e15ecd
LBDCode <42815395+LBDCode@users.noreply.github.com>
2020-01-18 19:57:08 -0500

    added maplayer2

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, Component} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { loadCss } from 'esri-loader';
@@ -7,14 +7,22 @@ import Dashboard from './components/Dashboard2';
 import SlimNav from './components/SlimNav';
 import Nav from './components/Nav';
 import Legend from './components/Legend'
-import  MapLayer from "./components/MapLayer/index"
+// import  MapLayer from "./components/MapLayer/index"
+import Button from 'react-bootstrap/Button'
+import  MapLayer2 from "./components/MapLayer2/index"
 
-
-const App = () => {
+class App  extends Component {
+  constructor(props) {
+    super(props);
+    // a ref to the DOM node where we want to create the map
+    // see: https://reactjs.org/docs/refs-and-the-dom.html
+    this.state = {
+      center: [-79.9414, 37.2710]
+    }
+  }
 
     // get esrii default css
-    loadCss();
-    
+
     
     //   return(<div>
     //   {/* create as sceneview w/ full page map TODO: move styling to index.css */}
@@ -35,19 +43,24 @@ const App = () => {
     //   <Legend />
     //   {/* <Sidebar></Sidebar> */}
     // </div>)
-    return (
-      <div>
-        <SlimNav></SlimNav>
-        <Nav />
-        {/* render mapLayer component, whch include the feature class and different basemap layer*/}
-        <MapLayer />     
-        {/* render dashboard component, pass selected gage info through*/}
-        <Dashboard />
-        <Legend />
-        {/* <Sidebar></Sidebar> */}
-      </div>
-      
-    );
+    render() {
+
+      return (
+        <div>
+          <SlimNav></SlimNav>
+          <Nav />
+          {/* render mapLayer component, whch include the feature class and different basemap layer*/}
+          <MapLayer2 center={this.state.center} />     
+          {/* render dashboard component, pass selected gage info through*/}
+          <Dashboard />
+          <Legend />
+          <Button id="clickTester" onClick={() => this.setState({center : [150.644, -34.397]})}variant="primary">Primary</Button>
+                  {/* <Sidebar></Sidebar> */}
+        </div>
+        
+      );
+  
+    }
 }
 
 export default App;
